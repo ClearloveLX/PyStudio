7618b13856235b92edebee356a5d0af8a825ed4c
丶 Clearlove <gk1213656215@outlook.com>
2017-08-22 20:25:31 +0800

    添加项目文件。

---
+++ PyStudio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PyStudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
